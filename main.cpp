bfae5e0a96d60423621a570f3577a3f1c38a8f56
Scotty Gomez <scottygomez007@gmail.com>
2025-03-28 14:14:53 -0700

    Qt hello world environment check

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <QCoreApplication>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
+    QCoreApplication app(argc, argv);
     std::cout << "Hello World" << std::endl;
 
-    return 0;
+    return app.exec();
 }
